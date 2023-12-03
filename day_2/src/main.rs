34cba047da017bb8aa63e7f1776da0b2db2c3695
TheLeeeo <leonard.e.forsberg@gmail.com>
2023-12-03 17:57:05 +0100

    day_2: part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -2,26 +2,22 @@ use scanf::sscanf;
 
 const INPUT: &str = include_str!("input.txt");
 
-const MAX_RED: u32 = 12;
-const MAX_GREEN: u32 = 13;
-const MAX_BLUE: u32 = 14;
-
 fn main() {
     let mut games: Vec<Game> = Vec::new();
     INPUT.lines().for_each(|line| {
         let game = parse_line(line);
-        match game {
-            Some(game) => games.push(game),
-            None => (),
-        }
+        games.push(game);
     });
 
-    let id_sum = games.iter().fold(0, |acc, game| acc + game.id);
+    let mut power_sum: u32 = 0;
+    for game in games {
+        power_sum += game.max_red * game.max_green * game.max_blue;
+    }
 
-    println!("Sum of ids: {}", id_sum);
+    println!("Sum of power: {}", power_sum);
 }
 
-fn parse_line(line: &str) -> Option<Game> {
+fn parse_line(line: &str) -> Game {
     let mut id: u32 = 0;
     let mut rest = String::new();
     sscanf!(line, "Game {}:{}", id, rest).unwrap();
@@ -30,32 +26,47 @@ fn parse_line(line: &str) -> Option<Game> {
 
     let variants = get_game_variants(&rest);
 
+    let mut max_red: u32 = 0;
+    let mut max_green: u32 = 0;
+    let mut max_blue: u32 = 0;
+
     for variant in variants {
-        match parse_variant(variant) {
-            Some(_) => (),
-            None => return None,
+        let cc = parse_variant(variant);
+
+        if cc.red > max_red {
+            max_red = cc.red;
+        }
+        if cc.green > max_green {
+            max_green = cc.green;
+        }
+        if cc.blue > max_blue {
+            max_blue = cc.blue;
         }
     }
 
-    Some(Game { id })
+    Game {
+        max_red,
+        max_green,
+        max_blue,
+    }
 }
 
-fn get_game_variants<'a>(game: &'a String) -> Vec<&'a str> {
+fn get_game_variants(game: &str) -> Vec<&str> {
     let mut variants: Vec<&str> = Vec::new();
 
-    game.split(";").for_each(|variant| {
+    game.split(';').for_each(|variant| {
         variants.push(variant);
     });
 
     variants
 }
 
-fn parse_variant(variant: &str) -> Option<ColorCount> {
+fn parse_variant(variant: &str) -> ColorCount {
     let mut red: u32 = 0;
     let mut green: u32 = 0;
     let mut blue: u32 = 0;
 
-    let color_variants = variant.split(",");
+    let color_variants = variant.split(',');
 
     for cv in color_variants {
         let mut color = String::new();
@@ -69,33 +80,21 @@ fn parse_variant(variant: &str) -> Option<ColorCount> {
         }
 
         match color.as_str() {
-            "red" => {
-                if value > MAX_RED {
-                    return None;
-                }
-                red += value
-            }
-            "green" => {
-                if value > MAX_GREEN {
-                    return None;
-                }
-                green += value
-            }
-            "blue" => {
-                if value > MAX_BLUE {
-                    return None;
-                }
-                blue += value
-            }
+            "red" => red += value,
+            "green" => green += value,
+            "blue" => blue += value,
             _ => panic!("Unknown color: {}", color),
         }
     }
 
-    Some(ColorCount { red, green, blue })
+    ColorCount { red, green, blue }
 }
 
+#[derive(Debug)]
 struct Game {
-    id: u32,
+    max_red: u32,
+    max_green: u32,
+    max_blue: u32,
 }
 
 struct ColorCount {
@@ -129,51 +128,48 @@ mod tests {
     #[test]
     fn test_parse_variant() {
         let variant = " 2 red";
-        let cc = parse_variant(variant).unwrap();
+        let cc = parse_variant(variant);
 
         assert_eq!(cc.red, 2);
         assert_eq!(cc.green, 0);
         assert_eq!(cc.blue, 0);
 
         let variant = " 2 red, 3 green";
-        let cc = parse_variant(variant).unwrap();
+        let cc = parse_variant(variant);
 
         assert_eq!(cc.red, 2);
         assert_eq!(cc.green, 3);
         assert_eq!(cc.blue, 0);
 
         let variant = " 2 red, 3 green, 4 blue";
-        let cc = parse_variant(variant).unwrap();
+        let cc = parse_variant(variant);
 
         assert_eq!(cc.red, 2);
         assert_eq!(cc.green, 3);
         assert_eq!(cc.blue, 4);
 
         let variant = " 14 red, 3 green, 4 blue";
-        let cc: Option<ColorCount> = parse_variant(variant);
-        match cc {
-            Some(_) => panic!("Should not be able to parse this"),
-            None => (),
-        }
+        let cc = parse_variant(variant);
+
+        assert_eq!(cc.red, 14);
+        assert_eq!(cc.green, 3);
+        assert_eq!(cc.blue, 4);
     }
 
     #[test]
     fn test_parse_line() {
-        let line = "Game 1: 2 green; 4 red";
-        let game = parse_line(line).unwrap();
-
-        assert_eq!(game.id, 1);
-
-        let line = "Game 4: 2 red, 3 green, 4 blue; 5 red, 6 green, 7 blue";
-        let game = parse_line(line).unwrap();
-
-        assert_eq!(game.id, 4);
-
         let line = "Game 1: 2 red, 20 green, 4 blue";
         let game = parse_line(line);
-        match game {
-            Some(_) => panic!("Should not be able to parse this"),
-            None => (),
-        }
+
+        assert_eq!(game.max_red, 2);
+        assert_eq!(game.max_green, 20);
+        assert_eq!(game.max_blue, 4);
+
+        let line = "Game 2: 2 red, 20 green, 4 blue; 3 red, 5 green, 6 blue";
+        let game = parse_line(line);
+
+        assert_eq!(game.max_red, 3);
+        assert_eq!(game.max_green, 20);
+        assert_eq!(game.max_blue, 6);
     }
 }
